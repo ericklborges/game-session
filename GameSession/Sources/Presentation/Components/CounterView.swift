5d6556f65824c64278c91b9fd956366b3f0c9ac8
Erick Borges <erick.borges@stone.com.br>
2023-02-02 19:25:53 -0300

    feat: add CounterView

---
+++ GameSession/Sources/Presentation/Components/CounterView.swift
@@ -0,0 +1,38 @@
+//
+//  CounterView.swift
+//  GameSession
+//
+//  Created by Erick Lozano Borges on 31/01/23.
+//  Copyright © 2023 bcl. All rights reserved.
+//
+
+import SwiftUI
+
+struct CounterView: View {
+   
+    private let textFont = GameSessionFontFamily.PixelSplitter.regular.font(size: 50)
+    
+    let text: String
+    
+    var body: some View {
+        ZStack(alignment: .center) {
+            Image(asset: GameSessionAsset.Assets.screen)
+                .resizable()
+                .aspectRatio(1, contentMode: .fit)
+            Text(text)
+                .foregroundColor(.init(asset: GameSessionAsset.Colors.textColor))
+                .font(.custom(textFont.fontName, size: textFont.pointSize))
+                .alignmentGuide(VerticalAlignment.center) { context in
+                    context.height
+                }
+        }
+    }
+}
+
+struct CounterView_Previews: PreviewProvider {
+    static var previews: some View {
+        CounterView(text: "+1")
+            .previewLayout(PreviewLayout.sizeThatFits)
+            .padding()
+    }
+}
