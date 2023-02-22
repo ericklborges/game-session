86d52b095b9fd4ead3a0bdc1f59e191f010cdd97
Erick Borges <erick.borges@stone.com.br>
2023-02-22 14:05:14 -0300

    feat: improve button arrow layout for larger devices

--- GameSession/Sources/Presentation/Components/CounterButton.swift
+++ GameSession/Sources/Presentation/Components/CounterButton.swift
@@ -34,17 +34,20 @@ struct CounterButton: View {
 fileprivate struct CounterButtonStyle: ButtonStyle {
 
     struct ImageSet {
+        let arrow: GameSessionImages
         let normal: GameSessionImages
         let pressed: GameSessionImages
         
         static let up = ImageSet(
-            normal: GameSessionAsset.Assets.buttonUpIdle,
-            pressed: GameSessionAsset.Assets.buttonUpPressed
+            arrow: GameSessionAsset.Assets.buttonArrowUp,
+            normal: GameSessionAsset.Assets.buttonBackgroundUpIdle,
+            pressed: GameSessionAsset.Assets.buttonBackgroundUpPressed
         )
         
         static let down = ImageSet(
-            normal: GameSessionAsset.Assets.buttonDownIdle,
-            pressed: GameSessionAsset.Assets.buttonDownPressed
+            arrow: GameSessionAsset.Assets.buttonArrowDown,
+            normal: GameSessionAsset.Assets.buttonBackgroundDownIdle,
+            pressed: GameSessionAsset.Assets.buttonBackgroundDownPressed
         )
     }
     
@@ -52,27 +55,44 @@ fileprivate struct CounterButtonStyle: ButtonStyle {
     
     func makeBody(configuration: Self.Configuration) -> some View {
         let imageSet: ImageSet = function == .add ? .up : .down
-        let image = configuration.isPressed ? imageSet.pressed : imageSet.normal
+        let backgroundImage = configuration.isPressed ? imageSet.pressed : imageSet.normal
+        let arrowImage = imageSet.arrow
          
         if configuration.isPressed {
             UIImpactFeedbackGenerator(style: .medium).impactOccurred()
         } else {
             UIImpactFeedbackGenerator(style: .soft).impactOccurred()
         }
-            
-        return Image(asset: image)
-            .resizable()
-            .frame(maxHeight: 200)
-            .scaleEffect(x: 1, y: configuration.isPressed ? 0.95 : 1, anchor: .bottom)
+
+        return ZStack {
+            Image(asset: backgroundImage)
+                .resizable()
+
+            Image(asset: arrowImage)
+                .resizable()
+                .scaledToFit()
+                .padding([.top, .bottom], 44)
+        }
+        .frame(maxHeight: 200)
+        .scaleEffect(x: 1, y: configuration.isPressed ? 0.95 : 1, anchor: .bottom)
+
     }
 }
 
 struct CounterButton_Previews: PreviewProvider {
     static var previews: some View {
+        previewOnDevice("iPhone 14 Pro")
+        previewOnDevice("iPhone 13 mini")
+        previewOnDevice("iPhone 8 Plus")
+        previewOnDevice("iPhone SE (3rd generation)")
+    }
+
+    static func previewOnDevice(_ previewDevice: PreviewDevice? = nil) -> some View {
         VStack(spacing: 32.0) {
             CounterButton(function: .add, action: { print("action") })
             CounterButton(function: .subtract, action: { print("action") })
         }
         .padding()
+        .previewDevice(previewDevice)
     }
 }
